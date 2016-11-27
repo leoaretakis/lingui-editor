37438eb0035e2d93721117c52e27af438835d5c0
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-11-27 18:58:39 +0100

    feat(api): Add GET /api/resources endpoint
    Endpoint lists all available resources (message catalogues) in available languages

---
+++ src/resources.js
@@ -0,0 +1,40 @@
+import fs from "fs"
+import path from "path"
+
+
+function groupByLanguage(files) {
+  const langRe = /(.*)([a-z]{2})\.json/
+
+  return (
+    files
+    .map((filename) => langRe.exec(filename))
+    .reduce((acc, item) => {
+      const {1: basename, 2: lang, input} = item
+      if (!acc[basename]) acc[basename] = {}
+
+      acc[basename][lang] = input
+
+      return acc
+    }, {})
+  )
+}
+
+
+function loadResources(pathname) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(pathname, (err, files) => {
+      if (err) {
+        reject(new Error(err))
+      } else {
+        resolve(
+          groupByLanguage(
+            files .map((filename) => path.join(pathname, filename))
+          )
+        )
+      }
+    })
+  })
+}
+
+
+export { loadResources }
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { loadResources } from "./resources.js"
+
+const app = express()
+
+app.get('/api/resources', (req, res) => {
+  loadResources("./example")
+    .then(JSON.stringify)
+    .then((json) => res.send(json))
+})
+
+app.listen(3000, () => {
+  console.log('lingui-translator running at http://localhost:3000')
+})
